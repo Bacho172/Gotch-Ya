96558691c5643933b8a967bae6dcd87db2835799
Damian-kw <logan560@wp.pl>
2018-12-04 14:24:01 +0100

    implementing accelometer class and developer mode to test sensors

--- app/src/main/java/com/example/adars/gotchya/MainActivity.java
+++ app/src/main/java/com/example/adars/gotchya/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.adars.gotchya;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -10,9 +13,10 @@ import com.example.adars.gotchya.Core.Fonts;
 import com.example.adars.gotchya.Core.Functions;
 import com.example.adars.gotchya.DataModel.DataModel.UserModel;
 import com.example.adars.gotchya.DataModel.DomainModel.User;
+import com.example.adars.gotchya.Sensors.TestAccelometerActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonDeveloperMode;
     private ImageButton imageButtonGetStarted;
 
     private TextView textViewDescription1;
@@ -22,21 +26,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        buttonDeveloperMode=findViewById(R.id.button_developer_mode);
         textViewDescription1 = findViewById(R.id.textViewDescription);
         textViewDescription2 = findViewById(R.id.textViewDescription2);
         Functions.setFont(this, textViewDescription1, Fonts.FONT_AVENIR_NextLTProRegular);
         Functions.setFont(this, textViewDescription2, Fonts.FONT_AVENIR_NextLTProRegular);
-
         imageButtonGetStarted = findViewById(R.id.imageButtonGetStarted);
         imageButtonGetStarted.setOnClickListener((l) -> imageButtonGetStartedClick());
-
+        buttonDeveloperMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent= new Intent(getApplicationContext(),TestAccelometerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
+    private void setButtonDeveloperModeClick(){
+        startActivity(new Intent(this,TestAccelometerActivity.class));
+    }
     private void imageButtonGetStartedClick() {
         User rememberedUser = Functions.loadUserData(this);
         if (rememberedUser != null) UserModel.getInstance().logIn(rememberedUser);
         startActivity(new Intent(this, rememberedUser != null ? MainMenuActivity.class : LogInActivity.class));
     }
-
 }
---
+++ app/src/main/java/com/example/adars/gotchya/Sensors/TestAccelometerActivity.java
@@ -0,0 +1,15 @@
+package com.example.adars.gotchya.Sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adars.gotchya.R;
+
+public class TestAccelometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_accelometer);
+    }
+}
