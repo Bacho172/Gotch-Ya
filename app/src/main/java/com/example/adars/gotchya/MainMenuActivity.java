656931fc78fa7687ad3cca4877587b9c448bda50
Bacho172 <adarsim1@op.pl>
2018-12-28 15:37:52 +0100

    Update MainMenuActivity title

--- app/src/main/java/com/example/adars/gotchya/MainMenuActivity.java
+++ app/src/main/java/com/example/adars/gotchya/MainMenuActivity.java
@@ -34,7 +34,7 @@ public class MainMenuActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-//        setTitle("Witaj '" + UserModel.getInstance().getCurrentUser().getLogin() + "'");
+        setTitle(R.string.app_name);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
