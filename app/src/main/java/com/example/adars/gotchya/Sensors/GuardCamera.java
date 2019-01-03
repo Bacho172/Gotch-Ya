2b476bb9c9faf2de627304b6e30fc515c8a18152
Damian-kw <logan560@wp.pl>
2019-01-03 13:39:31 +0100

    implementing camera module

--- app/src/main/java/com/example/adars/gotchya/Sensors/GuardCamera.java
+++ app/src/main/java/com/example/adars/gotchya/Sensors/GuardCamera.java
@@ -1,14 +1,49 @@
 package com.example.adars.gotchya.Sensors;
-import android.graphics.Camera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
 
 public class GuardCamera {
-    public static Camera getCameraInstance(){
-        Camera c = null;
+    Camera camera;
+    Context context;
+    Camera.Parameters parameters;
+    SurfaceHolder sHolder;
+
+GuardCamera(){
+
+}
+    void takePHoto(Context context) {
+        camera = null;
+        this.context = context;
         try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
+            camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK); // attempt to get a Camera  back instance
+        } catch (Exception e) {
             // Camera is not available (in use or does not exist)
         }
-        return c; // returns null if camera is unavailable
-    }}
+        SurfaceView sv = new SurfaceView(this.context);
+        try {
+            camera.setPreviewDisplay(sv.getHolder());
+            parameters = camera.getParameters();
+            camera.setParameters(parameters);
+            camera.startPreview();
+
+            camera.takePicture(null, null, mCall);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sHolder = sv.getHolder();
+        sHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+        Camera.PictureCallback mCall=new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                //to do
+            }
+        }
+
+}
